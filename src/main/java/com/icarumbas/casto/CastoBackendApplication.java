d15a91c6f72477a7f9fd598bf154635e87db244e
icarumbas <maksim.ternovtsi@yahoo.com>
2023-03-20 14:34:28 +0100

    Initial commit

---
+++ src/main/java/com/icarumbas/casto/CastoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.icarumbas.casto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icarumbas/casto/CastoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icarumbas.casto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CastoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
