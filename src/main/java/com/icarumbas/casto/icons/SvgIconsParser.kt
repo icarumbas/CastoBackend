b0d66c4faf2064136dfff00dd4b9f5d61248b0e6
icarumbas <maksim.ternovtsi@yahoo.com>
2023-03-21 01:08:45 +0100

    WIP

--- src/main/java/com/icarumbas/casto/icons/SvgIconsParser.kt
+++ src/main/java/com/icarumbas/casto/icons/SvgIconsParser.kt
@@ -3,6 +3,7 @@ package com.icarumbas.casto.icons
 import com.icarumbas.casto.utils.SvgParser
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.stereotype.Component
 import java.io.IOException
 import java.nio.file.FileVisitResult
@@ -20,7 +21,7 @@ class SvgIconsParser @Autowired constructor(
     private val svgParser: SvgParser,
 ) {
     fun run() {
-        val svgDir = Path(ClassPathResource("icons/svg").path)
+        val svgDir = DefaultResourceLoader().getResource("icons/svg").file.toPath()
 
         Files.walkFileTree(svgDir, object : SimpleFileVisitor<Path>() {
             override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
@@ -36,7 +37,7 @@ class SvgIconsParser @Autowired constructor(
                 return FileVisitResult.CONTINUE
             }
 
-            override fun postVisitDirectory(dir: Path, exc: IOException): FileVisitResult {
+            override fun postVisitDirectory(dir: Path, exc: IOException?): FileVisitResult {
                 return FileVisitResult.TERMINATE
             }
         })
